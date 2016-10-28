4c2c772efee28c7ab0af1f66f5ffa24cf974047b
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-10-28 14:20:45 -0700

    [refactor] - minor changes

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -45,7 +45,8 @@ authController.SIGNUP = function (req, res) {
         password: req.body.password
       })
       .then(function(user) {
-        var token = jwt.sign({ email: req.body.email }, 'this is the secret token!');
+        var token = jwt.sign({ email: req.body.email, expiresIn: Math.floor(Date.now() / 1000) + (60 * 60) }, 'this is the secret token!');
+
         res.status(200).header('Auth', token).header('currentUser', user.id).send({ token:token, user: user.id })
       })
       .catch(function(err) {
@@ -60,11 +61,12 @@ authController.SIGNIN = function (req, res) {
     "email": req.body.email
   })
   .then(function (user) {
-    bcrypt.compare(req.body.password, result.password, (err, response) => {
+    bcrypt.compare(req.body.password, user.password, (err, response) => {
       if (err) {
         res.status(500).send(err)
       } else if (response !== null) {
-        var token = jwt.sign({ email: req.body.email }, 'this is the secret token!');
+        var token = jwt.sign({ email: req.body.email, expiresIn: Math.floor(Date.now() / 1000) + (60 * 60) }, 'this is the secret token!');
+
         res.status(200).header('Auth', token).header('currentUser', user.id).send({ token:token, user: user.id })
       } else {
         res.status(400).send('Invalid email or password')
